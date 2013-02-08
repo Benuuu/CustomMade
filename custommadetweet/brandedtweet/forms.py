1a5d779c0d00d7c398dd7141cdb09c8aa54abb15
Benuuu <ben@benuuu.com>
2013-02-08 14:56:16 -0500

    character count.  fixed the permission redirect issue.

--- custommadetweet/brandedtweet/forms.py
+++ custommadetweet/brandedtweet/forms.py
@@ -1,4 +1,10 @@
 from django import forms
 
 class BrandedTweetForm(forms.Form):
-    content = forms.CharField(max_length=140, widget=forms.TextInput(attrs={'size':140}))
+    max_len = 140
+    # both keyup and keydown because keypress doesn't detect delete
+    attrs={'size':140,
+           'onkeyup': 'return textCounter(this, this.form.counter, %d);' % max_len,
+           'onkeydown': 'return textCounter(this, this.form.counter, %d);' % max_len
+          }
+    content = forms.CharField(max_length=max_len, widget=forms.TextInput(attrs=attrs))
--- custommadetweet/brandedtweet/views.py
+++ custommadetweet/brandedtweet/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render_to_response, get_object_or_404, redirect
-from django.http import Http404, HttpResponse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required,user_passes_test
-from django.utils import timezone
 from django.core.context_processors import csrf
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import Http404, HttpResponse
+from django.shortcuts import render_to_response, redirect
+from django.utils import timezone
 
 from brandedtweet.models import BrandedTweet
 from brandedtweet.forms import BrandedTweetForm
@@ -27,14 +27,17 @@ def index(request):
 
 
 @login_required(login_url='/login/')
-@user_passes_test(is_editor)
 def editor_view(request):
+    if not is_editor(request.user):
+        return HttpResponse('Unauthorized', status=401)
     return render_to_response('brandedtweet/editor.html')
 
 
 @login_required(login_url='/login/')
-@user_passes_test(is_editor)
 def blocked_tweets_view(request):
+    if not is_editor(request.user):
+        return HttpResponse('Unauthorized', status=401)
+        
     tweets = paginated_tweets(request, is_dirty=True)
 
     c = {'tweet_list': tweets, 'is_dirty': True, 'type_tweets': 'Blocked'}
@@ -45,8 +48,10 @@ def blocked_tweets_view(request):
 
 
 @login_required(login_url='/login/')
-@user_passes_test(is_editor)
 def published_tweets_view(request):
+    if not is_editor(request.user):
+        return HttpResponse('Unauthorized', status=401)
+
     tweets = paginated_tweets(request, is_dirty=False)
 
     c = {'tweet_list': tweets, 'is_dirty': False, 'type_tweets': 'Published'}
@@ -56,7 +61,6 @@ def published_tweets_view(request):
     return render_to_response('brandedtweet/list.html', c)
 
 
-@user_passes_test(is_editor)
 def paginated_tweets(request, tweets_per_page=10, is_dirty=False):
     tweet_list = BrandedTweet.objects.filter(is_dirty=is_dirty).order_by('-submit_date')
     paginator = Paginator(tweet_list, tweets_per_page)
@@ -73,8 +77,10 @@ def paginated_tweets(request, tweets_per_page=10, is_dirty=False):
 
 
 @login_required(login_url='/login/')
-@user_passes_test(is_staff)
 def staff_view(request):
+    if not is_staff(request.user):
+        return HttpResponse('Unauthorized', status=401)
+    
     cmf = CustomMadeFilter()
     c = {}
     c.update(csrf(request))
